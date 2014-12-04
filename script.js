60d8657ce16df24df47ca06ed3270a62317ac71d
guillermochica <1bgchica@gmail.com>
2014-12-04 14:44:03 +0100

    v1 added

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+$('document').ready(function() {
+    $('#submit').click(function() {
+        var title = $('#film').val();
+
+        $.get( 'http://www.omdbapi.com/?t=' + title + '&y=&plot=short&r=json', function( data ) {
+          datos = JSON.parse(data);
+          $( "#title" ).html('<b>Title: </b>' +  datos.Title);
+          $("#year").html('<b>Year: </b>' +  datos.Year);
+          $("#director").html('<b>Director: </b>' +  datos.Director);
+          $("#actors").html('<b>Actors: </b>' +  datos.Actors);
+          $("#country").html('<b>Country: </b>' +  datos.Country);
+          $("#language").html('<b>Language: </b>' +  datos.Language);
+          $("#runtime").html('<b>Runtime: </b>' +  datos.Runtime);
+          $("#plot").html('<b>Plot: </b>' +  datos.Plot);
+      });
+    });
+
+});
