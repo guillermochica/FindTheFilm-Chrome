41e6f881ec7957deb6993e80fe999d7c571b9bba
guillermochica <1bgchica@gmail.com>
2014-12-27 16:36:16 +0100

    Now it makes a search only if you put a movie

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ $('document').ready(function() {
     $('#submit').click(function() {
         var title = $('#film').val();
         var year = $('#inputyear').val();
-
-        $.get( 'http://www.omdbapi.com/?t=' + title + '&y=' + year + '&plot=short&r=json', function( data ) {
-          datos = JSON.parse(data);
-          $( "#title" ).html('<b>Title: </b>' +  datos.Title);
-          $("#year").html('<b>Year: </b>' +  datos.Year);
-          $("#director").html('<b>Director: </b>' +  datos.Director);
-          $("#actors").html('<b>Actors: </b>' +  datos.Actors);
-          $("#country").html('<b>Country: </b>' +  datos.Country);
-          $("#language").html('<b>Language: </b>' +  datos.Language);
-          $("#runtime").html('<b>Runtime: </b>' +  datos.Runtime);
-          $("#plot").html('<b>Plot: </b>' +  datos.Plot);
-          var string = '<img src= "' + datos.Poster + '" width="150" height="211"/>';
-          $("#poster").html(string)
+        if (title != '') {
+          $.get( 'http://www.omdbapi.com/?t=' + title + '&y=' + year + '&plot=short&r=json', function( data ) {
+            datos = JSON.parse(data);
+            $( "#title" ).html('<b>Title: </b>' +  datos.Title);
+            $("#year").html('<b>Year: </b>' +  datos.Year);
+            $("#director").html('<b>Director: </b>' +  datos.Director);
+            $("#actors").html('<b>Actors: </b>' +  datos.Actors);
+            $("#country").html('<b>Country: </b>' +  datos.Country);
+            $("#language").html('<b>Language: </b>' +  datos.Language);
+            $("#runtime").html('<b>Runtime: </b>' +  datos.Runtime);
+            $("#plot").html('<b>Plot: </b>' +  datos.Plot);
+            var string = '<img src= "' + datos.Poster + '" width="150" height="211"/>';
+            $("#poster").html(string)
           });
+        }
     });
 
 });
